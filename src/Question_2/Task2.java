5cd3b57ad6a2d7a431f3fa534fd4b931dd516981
asus <s.pashkovskiy08@gmail.com>
2019-10-01 19:43:33 +0300

    ProgrammingWithClasses--1.2

---
+++ src/Question_2/Task2.java
@@ -0,0 +1,47 @@
+//Создйте  класс  Test2  двумя переменными. Добавьте  конструктор с входными параметрами. Добавьте
+// конструктор, инициализирующий члены класса по умолчанию. Добавьте set- и get- методы для полей экземпляра класса.
+package Question_2;
+
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+public class Task2 {
+
+    private String name;
+    private int age;
+
+    public Task2(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Task2() { //конструктор по умолчанию
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        Task2 Person1 = new Task2("Виктор",30);
+        Task2 Person2 = new Task2(); //сработает конструктор по умолчанию
+        System.out.println(Person1.age);
+        System.out.println(Person1.name);
+        Person1.setAge(18);
+        System.out.println(Person1.getAge()); //с помощью сеттеров и геттеров изменим переменные.
+        Person1.setName("Петр");
+        System.out.println(Person1.getName());
+    }
+}
